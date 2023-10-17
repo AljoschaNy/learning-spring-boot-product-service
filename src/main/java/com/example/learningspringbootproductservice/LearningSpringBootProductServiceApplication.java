8ee5ac94ca437707d0401b80f6913d667fe84839
Aljoscha Nyang <a.nyang@gmx.de>
2023-10-17 22:03:24 +0200

    Initial commit

---
+++ src/main/java/com/example/learningspringbootproductservice/LearningSpringBootProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.learningspringbootproductservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringBootProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearningSpringBootProductServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/learningspringbootproductservice/LearningSpringBootProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learningspringbootproductservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningSpringBootProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
